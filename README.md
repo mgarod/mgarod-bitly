745d1f6df4113ce6616f7aa6161decfdffa4c170
Michael Garod <mgarod@gmail.com>
2018-09-30 15:54:35 -0400

    Update README.md

--- README.md
+++ README.md
@@ -8,12 +8,41 @@ source venv/bin/activate
 pip install -r requirements.txt
 ```
 
+## Setup local server
+Simply run `./app.py`
+```
+| $ ./app.py -h
+usage: app.py [-h] [-d]
 
+Extension to Bitly API created by Michael Garod
+
+optional arguments:
+  -h, --help   show this help message and exit
+  -d, --debug  Start Flask in debug mode
+```
+Once launched, the server is accessible by `http://localhost:5000`
+
+## Endpoints
+This API repackages several existing `https://api-ssl.bitly.com/v4/` endpoints in order to create the new endpoint `/user/average`.
+
+Those endpoints that have simply been wrapped are:
+- `/v4/user`
+- `/v4/bitlinks/{bitlink}/countries`
+- `/v4/groups/{group_guid}/bitlinks`
+
+## Usage
+One the server is running, it may be accessed using `curl`. To access the challenge endpoint, use an Access Token provided by Bitly, and excute the following from the command line:
+```
+curl -H "Authorization: Bearer $TOKEN" http://localhost:5000/v4/user/average
+```
 
 ### Assumptions
 https://dev.bitly.com/v4/#operation/getMetricsForBitlinkByCountries
 - Given that I only have access to my own bitlinks, I have no data with a timestamp other than 9/30. Therefore, I am assuming that by passing in `?units=30&unit=day&size=30` (or some combination therein) to this endpoint, this query is capable of returning to me the last 30 days worth of metrics.
-- Given that I can only produce clicks from the `US`, I must assume that this endpoint will produce the field `metrics` which is an array of dicts. I am only able to view `"metrics":[{"value":"US","clicks":4}]`, but I am assuming it could look like `"metrics":[{"value":"US","clicks":4},{"value":"GB","clicks":6},{"value":"JP","clicks":3}]`
+- Given that I can only produce clicks from the `US`, I must assume that this endpoint will produce the field `metrics` which is an array of dicts. I am only able to view `"metrics":[{"value":"US","clicks":4}]`, but I am assuming it would look like `"metrics":[{"value":"US","clicks":4},{"value":"GB","clicks":6},{"value":"JP","clicks":3}]`
 
 Pagination
-- Given that I have so little data to play with while creating this API, I will be ignoring the more complex feature of pagination
+- Given that I have so little data to experiment with while creating this API, I will be ignoring the more complex feature of pagination
+
+### Testing
+Without proper data to explore, testing has been omitted. Because of this, the challenge enpoint is not guaranteed to function on any user with more than 50 bitlinks (due to lack of pagination processing), and any user that has clicks from more than 1 country.
--- app.py
+++ app.py
@@ -9,28 +9,30 @@ from flask import Flask, jsonify, abort, request
 import requests
 from requests.auth import HTTPBasicAuth
 
-from helpers import init_app
 
+app = Flask(__name__)
+
+parser = argparse.ArgumentParser(description='Extension to Bitly API created by Michael Garod')
+parser.add_argument('-d', '--debug',
+                    action='store_true',
+                    help='Start Flask in debug mode',
+                    default=False)                    
+args = parser.parse_args()
 
 logging.basicConfig(
     format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s] %(message)s",
     handlers=[
         logging.FileHandler("{0}/{1}.log".format('.', 'app')),
         logging.StreamHandler()
-    ],
-    level=logging.DEBUG)
-app, args = init_app()
+    ])
+
+# API endpoint used for all calls to Bitly
 v4_api = 'https://api-ssl.bitly.com/v4'
-
-
-@app.route('/')
-def index():
-    return jsonify({'hello': 'world'})
 
 
 # https://api-ssl.bitly.com/v4/user
 # curl -H "Authorization: Bearer <oauth_token>" http://localhost:5000/user
-@app.route('/user', methods=['GET'])
+@app.route('/v4/user', methods=['GET'])
 def get_user():
     endpoint = f'{v4_api}/user'
     auth_header = request.headers.get('Authorization')
@@ -38,28 +40,19 @@ def get_user():
     return return_response(response)
 
 
-# https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks
-# curl -H "Authorization: Bearer <oauth_token>" http://localhost:5000/bitlinks/<domain>/<link>/clicks
-@app.route('/bitlinks/<string:domain>/<string:link>/clicks', methods=['GET'])
-def get_clicks(domain, link):
-    endpoint = f'{v4_api}/bitlinks/{domain}/{link}/clicks'
-    auth_header = request.headers.get('Authorization')
-    response = make_call(endpoint, auth_header, payload=request.args)
-    return return_response(response)
-
-
 # https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/countries
-# curl -H "Authorization: Bearer <oauth_token>" http://localhost:5000/bitlinks/<domain>/<link>/countries
-@app.route('/bitlinks/<string:domain>/<string:link>/countries', methods=['GET'])
+# curl -H "Authorization: Bearer <oauth_token>" http://localhost:5000/v4/bitlinks/<domain>/<link>/countries
+@app.route('/v4/bitlinks/<string:domain>/<string:link>/countries', methods=['GET'])
 def get_clicks_by_country(domain, link):
     endpoint = f'{v4_api}/bitlinks/{domain}/{link}/countries'
     auth_header = request.headers.get('Authorization')
     response = make_call(endpoint, auth_header, payload=request.args)
     return return_response(response)
 
+
 # https://api-ssl.bitly.com/v4/groups/{group_guid}/bitlinks
-# curl -H "Authorization: Bearer <oauth_token>" http://localhost:5000/groups/<guid>/bitlinks
-@app.route('/groups/<string:guid>/bitlinks', methods=['GET'])
+# curl -H "Authorization: Bearer <oauth_token>" http://localhost:5000/v4/groups/<guid>/bitlinks
+@app.route('/v4/groups/<string:guid>/bitlinks', methods=['GET'])
 def get_bitlinks_by_group(guid):
     endpoint = f'{v4_api}/groups/{guid}/bitlinks'
     auth_header = request.headers.get('Authorization')
@@ -67,29 +60,22 @@ def get_bitlinks_by_group(guid):
     return return_response(response)
 
 
-# https://api-ssl.bitly.com/v4/groups/{group_guid}/countries
-# curl -H "Authorization: Bearer <oauth_token>" http://localhost:5000/groups/<guid>/countries
-@app.route('/groups/<string:guid>/countries', methods=['GET'])
-def get_metrics_for_group_by_countries(guid):
-    endpoint = f'{v4_api}/groups/{guid}/countries'
-    auth_header = request.headers.get('Authorization')
-    response = make_call(endpoint, auth_header, payload=request.args)
-    return return_response(response)
-
-
-# the average user clicks per country over the last 30 days for a user's default group.
 '''
+Task: The average user clicks per country over the last 30 days for a user's default group.
+
 1) Get user's guid
 2) Get all bitlinks in guid
 3) For every bitlink in guid
     3.1) add to a defaultdict clicks_by_country where keys are Country and values are clicks
 4) Reduce the clicks_by_country by dividing each value by 30
+
+curl -H "Authorization: Bearer <oauth_token>" http://localhost:5000/v4/user/average
 '''
-@app.route('/user/average')
+@app.route('/v4/user/average')
 def get_average_clicks_for_group_by_country():
     user = get_user()
     user_default_guid = user.json['default_group_guid']
-    logging.debug(f'user: {user_default_guid}')
+    logging.debug(f'user_default_guid: {user_default_guid}')
 
     bitlinks = get_bitlinks_by_group(user_default_guid)
     # TODO: Pagination when group has more than 50 links
@@ -104,8 +90,8 @@ def get_average_clicks_for_group_by_country():
             clicks_by_country[metric['value']] += int(metric['clicks'])
 
     avg_clicks_by_country = {k: v/30.0 for k, v in clicks_by_country.items()}
-    logging.debug(f'clicks_by_country: {clicks_by_country}')
-    logging.debug(f'avg_clicks_by_country: {avg_clicks_by_country}')
+    logging.debug(f'total clicks_by_country: {clicks_by_country}')
+    logging.debug(f'avg_clicks_by_country over 30 days: {avg_clicks_by_country}')
     return jsonify(avg_clicks_by_country)
 
 
@@ -114,7 +100,6 @@ def make_call(endpoint, auth_header, payload={}):
     response = requests.get(endpoint, headers={'Authorization': auth_header}, params=payload)
     logging.debug(f'Response to {endpoint} was {response.text}')
     return response
-
 
 
 def return_response(response):
